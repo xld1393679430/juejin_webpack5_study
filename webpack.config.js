df8ff7da2e6fb8370cbcf49f61283d0a7c50abad
ld_xu <ld_xu@trip.com>
2022-11-09 10:22:17 +0800

    安装eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,6 +8,7 @@ module.exports = {
     path: path.join(__dirname, "./dist"),
   },
   mode: "development",
+  devtool: false,
   module: {
     rules: [
       {
@@ -22,11 +24,21 @@ module.exports = {
       },
       {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-typescript"],
+            },
+          },
+        ],
       },
     ],
   },
   resolve: {
     extensions: [".ts", ".js"], // 使用 resolve.extensions 声明自动解析 .ts 后缀文件，这意味着代码如 import "./a.ts" 可以忽略后缀声明，简化为 import "./a" 文件
   },
+  plugins: [
+    new ESLintPlugin({ extensions: [".js", ".ts"] }), // 添加 eslint-webpack-plugin 插件实例
+  ],
 };
